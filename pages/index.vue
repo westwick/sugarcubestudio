ae56827695a42a4f72b175abecc00b93f80aaf48
Andrew Westwick <awestwick@gmail.com>
2024-09-02 20:35:29 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Home</h1>
+    <p>Uhhhh</p>
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <p>Uhhhh</p>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style></style>
