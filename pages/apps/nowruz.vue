a133b1eb53a67ca5767d578c2e9ad0197e7ebaec
Andrew Westwick <awestwick@gmail.com>
2024-09-08 02:22:34 -0500

    set og:site_name, use absolute path for image

--- pages/apps/nowruz.vue
+++ pages/apps/nowruz.vue
@@ -8,13 +8,14 @@
 useHead({
   meta: [
     { name: 'description', content: 'Test description for Nowruz.' },
+    { name: 'og:site_name', content: 'Sugar Cube Studio' },
 
     // Open Graph tags
     { property: 'og:title', content: 'Nowruz App by Sugar Cube Studio' },
     { property: 'og:description', content: 'SEO text for Nowruz' },
     { property: 'og:url', content: 'https://sugarcubestudio.netlify.app/apps/nowruz' },
     { property: 'og:type', content: 'website' },
-    { property: 'og:image', content: '/images/nowruz.png' },
+    { property: 'og:image', content: 'https://sugarcubestudio.netlify.app/images/nowruz.png' },
     { property: 'og:image:type', content: 'image/png' },
     { property: 'og:image:width', content: '256' },
     { property: 'og:image:height', content: '256' },
@@ -23,7 +24,7 @@ useHead({
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: 'My Custom Page Title' },
     { name: 'twitter:description', content: 'This is a custom page description for SEO and link previews.' },
-    { name: 'twitter:image', content: '/images/nowruz.png' },
+    { name: 'twitter:image', content: 'https://sugarcubestudio.netlify.app/images/nowruz.png' },
   ],
 })
 </script>
