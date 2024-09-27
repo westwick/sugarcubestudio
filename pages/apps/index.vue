9fa829634d575ff6052a915b1837c3e6a1bff9f8
bradkohl <bradkohl@gmail.com>
2024-09-27 07:50:48 -0700

    Create giant responsive  buttons for links ot apps

--- pages/apps/index.vue
+++ pages/apps/index.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="container mx-auto text-center">
+  <div class="container mx-auto text-center content-center">
     <h1 class="text-xl mb-4">Apps!</h1>
-    <p class="flex space-x-4 mx-auto">
-      <NuxtLink to="/apps/simurgh" class="link">Simurgh</NuxtLink>
-      <NuxtLink to="/apps/nowruz" class="link">Nowruz</NuxtLink>
+    <p class="flex pt-100 space-x-4 mx-auto">
+      <NuxtLink to="/apps/simurgh" class="link app-button flex-1"
+        >Simurgh</NuxtLink
+      >
+      <NuxtLink to="/apps/nowruz" class="link app-button flex-1"
+        >Nowruz</NuxtLink
+      >
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
 useHead({
-  title: 'Apps'
-})
+  title: "Apps",
+});
 </script>
 
 <style>
 .link {
   @apply text-yellow-300;
+}
+
+.app-button {
+  @apply bg-yellow-300 text-black px-40 py-40 rounded-md;
 }
 </style>
