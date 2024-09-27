86e3760b59d10cffe5412df89b00ad7c6aab8766
bradkohl <bradkohl@gmail.com>
2024-09-27 08:14:17 -0700

    Adds background image to Simurgh button and adjusts corresponding text for legibility

--- pages/apps/index.vue
+++ pages/apps/index.vue
@@ -2,7 +2,9 @@
   <div class="container mx-auto text-center content-center">
     <h1 class="text-xl mb-4">Apps!</h1>
     <p class="flex pt-100 space-x-4 mx-auto">
-      <NuxtLink to="/apps/simurgh" class="link app-button flex-1"
+      <NuxtLink
+        to="/apps/simurgh"
+        class="link app-button flex-1 simurgh-link outline-text"
         >Simurgh</NuxtLink
       >
       <NuxtLink to="/apps/nowruz" class="link app-button flex-1"
@@ -24,6 +26,20 @@ useHead({
 }
 
 .app-button {
-  @apply bg-yellow-300 text-black px-20 py-40 rounded-md;
+  @apply bg-yellow-300 text-black text-2xl px-20 py-40 rounded-md;
+}
+.simurgh-link {
+  background-image: url("/public/home-bg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  /* Add any other necessary styles */
+}
+
+.outline-text {
+  /* -webkit-text-stroke: 1px black; Outline width and color */
+  color: white; /* Text color */
+  text-shadow: 3px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000,
+    -1px 1px 0 #000, 1px 1px 0 #000; /*Additional outline effect */
 }
 </style>
